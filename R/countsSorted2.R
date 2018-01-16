64084e6f3fc936bf92bdbb8b4bbd27544461f172
jasonserviss <jason.serviss@ki.se>
2018-01-16 09:52:18 +0100

    adding documentation for the countsSorted2 and groupMeanPermutations datasets

---
+++ R/countsSorted2.R
@@ -0,0 +1,25 @@
+#' Counts and ERCC matrix for sorted cell lines.
+#'
+#' The HOS, HCT116, and A375 cell lines were FACS sorted, either as
+#' singlets or multiplets, and sequenced. Multiplets were sequenced into known
+#' combinations in order to know which connections are contained in each
+#' multiplet. This dataset is used in conjunction with the
+#' inst/sortedMultiplets20171116/*.Rmd analyses in the sp.scRNAseqTesting
+#' package.
+#'
+#' @docType data
+#' @name countsSorted2
+#' @format Two matrices, containing counts and ERCC counts, with:
+#' \describe{
+#' \item{rownames}{Gene names}
+#' \item{colnames}{Samples/cells}
+#' }
+#' @usage countsSorted2 or countsSortedERCC2
+#' @return matrix
+#' @examples
+#' countsSorted2
+#' countsSortedERCC2
+#'
+
+NULL
+
