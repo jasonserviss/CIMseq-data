b2702c915d2f94420612cce7680864602027c3a0
jasonserviss <jason.serviss@ki.se>
2018-03-16 19:55:16 +0100

    build again, think I found it

---
+++ inst/rawData/fetalPancreas/fetalPancreasCounts.R
@@ -0,0 +1,30 @@
+#run from package root
+#source('inst/rawData/fetalPancreas/fetalPancreasCounts.R')
+
+#fetalPancreasCounts
+#Should be 131 singlets and 69 multiplets.
+#Note: I never got the raw unfiltered counts.txt file for this from Martin.
+
+#read counts data
+counts <- read.table(
+  'inst/rawData/fetalPancreas/fetalPancreasCounts.txt',
+  header = TRUE,
+  sep = "\t"
+)
+
+#add prefix indicating singlets and multiplets
+counts <- labelSingletsAndMultiplets(counts, "1000102901")
+
+#extract ercc reads
+ercc <- detectERCCreads(counts)
+countsErccFp <- counts[ercc, ]
+countsFp <- counts[!ercc, ]
+
+#save data
+save(
+  countsFp,
+  countsErccFp,
+  file = 'data/fetalPancreasCounts.rda',
+  compress = 'bzip2'
+)
+
---
+++ inst/rawData/testingCounts/testingCounts.R
@@ -0,0 +1,57 @@
+#run from package root
+#source('./inst/rawData/testingCounts/testingCounts.R')
+#generates a data.frame of counts that resemble a small subset of real counts
+#data to be used for testing.
+
+#generate "normal" counts
+nCellTypes <- 2
+nCells <- 5
+nGenes <- 10
+
+synth <- sapply(1:nCellTypes, function(x) {
+  set.seed(x + 8923023)
+  rnbinom(nGenes * nCells, mu = 2^runif(nGenes, 0, 5), size = x)
+})
+
+counts <- matrix(as.numeric(synth), nrow = nGenes)
+
+#add low quality cell
+set.seed(989023)
+counts <- cbind(
+  counts,
+  matrix(rnbinom(nGenes, mu = 2^runif(nGenes, 0, 1), size = 1), ncol = 1)
+)
+
+#add low quality gene
+set.seed(83049)
+counts <- rbind(
+  counts,
+  matrix(rnbinom((nGenes * 3) + 3, mu = 2^runif(nGenes, 0, 1), size = 1), nrow = 3)
+)
+
+#add house keeping gene
+set.seed(232343999)
+counts <- rbind(
+  matrix(rnbinom(nGenes + 1, mu = 2^runif(nGenes, 5, 5), size = 1), nrow = 1),
+  counts
+)
+
+#add low quality based on house keeping gene
+counts[1, 5] <- 0
+
+#add names
+colnames(counts) <- paste0(LETTERS[1:ncol(counts)], ".htseq")
+rownames(counts) <- c("ACTB", letters[1:11], "ERCC-1", "__alignment_not_unique")
+
+#convert to data frame
+counts <- as.data.frame(counts)
+
+#Move gene names (rownames) to a column (instead of rownames) as is typical
+#in HTSeq output
+counts <- cbind(HGN = rownames(counts), counts)
+rownames(counts) <- 1:nrow(counts)
+
+#rename and save
+testingCounts <- counts
+save(testingCounts, file = "./data/testingCounts.rda", compress = "bzip2")
+
