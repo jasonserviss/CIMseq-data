95e6fe0df20cd8b280f6ff8634c15919420be9a8
jasonserviss <jason.serviss@ki.se>
2018-09-13 14:09:05 +0200

    another bug in scripts using new cell QC functions fixed

--- inst/rawData/countsRegev/counts_Regev.R
+++ inst/rawData/countsRegev/counts_Regev.R
@@ -48,8 +48,7 @@ counts <- counts[detectLowQualityGenes(counts), ]
 #remove low quality cells
 lqc.totalCounts <- detectLowQualityCells.totalCounts(counts, mincount = 1e4)
 lqc.housekeeping <- detectLowQualityCells.housekeeping(counts, geneName = "Actb", quantileCut = 0.01)
-lqc.ERCCfrac <- detectLowQualityCells.ERCCfrac(counts, countsERCC, percentile = 0.99)
-lqc <- lqc.totalCounts & lqc.housekeeping & lqc.ERCCfrac
+lqc <- lqc.totalCounts & lqc.housekeeping
 print(paste0("Removing a total of ", sum(!lqc), " cells based on the calculated metrics."))
 counts <- counts[, lqc]
 
